3950919dce15c2d5105cdfc79cdf53b92f51fb6b
webedivan <eayalaq14@unc.edu.pe>
2020-08-16 08:30:41 -0500

    ip direccion

--- botica/settings.py
+++ botica/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-if 'projects/boticaYaiza' in os.getcwd():
+if 'prueba/botica' in os.getcwd():
     ALLOWED_HOSTS = ['161.35.48.197']
